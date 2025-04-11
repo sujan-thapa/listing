7accf614467fbd015d4a6847d6525dda1bb07cec
Sujan Thapa <sujanthapamagar960@gmail.com>
2025-04-10 21:09:56 -0400

    task manager API

---
+++ taskManager/backend/db.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import pg from 'pg'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+// convert the module URL to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+// load the .env file from the root directory
+dotenv.config({path: path.resolve(__dirname, '../.env')})
+
+const {Pool} = pg;
+
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+
+})
+
+export default pool
